47de4dbd17a3d1dc0e1578f467b43672edca7e7c
2022sp93056 <126740090+2022sp93056@users.noreply.github.com>
2023-12-03 00:59:00 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:gptgen/chatgpt.dart';
+import 'package:provider/provider.dart';
+import 'package:gptgen/themes/theme_provider.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          return MaterialApp(
+            title: 'GPTGen',
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: const ChatGPTScreen(),
+          );
+        },
+      );
+}
