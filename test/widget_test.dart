29c15d7d2f5e204d81269d3b71319507df83df2f
arpitha93045 <127535994+arpitha93045@users.noreply.github.com>
2023-12-09 15:08:08 +0530

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gptgen/main.dart';
+import 'package:aichat/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
