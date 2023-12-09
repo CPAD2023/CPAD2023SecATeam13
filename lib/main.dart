2e2e6d06b8f773ba8eb7128a54d67a6c762a288f
2022sp93056 <126740090+2022sp93056@users.noreply.github.com>
2023-12-09 15:12:52 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,57 @@
+import 'dart:async';
+import 'package:aichat/components/HideKeyboard.dart';
+import 'package:aichat/page/AppOpenPage.dart';
+import 'package:aichat/stores/AIChatStore.dart';
 import 'package:flutter/material.dart';
-import 'package:gptgen/chatgpt.dart';
+import 'package:aichat/utils/Chatgpt.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:provider/provider.dart';
-import 'package:gptgen/themes/theme_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() async {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.black,
+  ));
+  await dotenv.load(fileName: ".env");
+
+  await GetStorage.init();
+  await ChatGPT.initChatGPT();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AIChatStore(),
+      child: const MyApp(),
+    ),
+  );
+  configLoading();
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return HideKeyboard(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          brightness: Brightness.light,
+        ),
+        home: const SplashPage(),
+        builder: EasyLoading.init(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => ThemeProvider(),
-        builder: (context, _) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
-          return MaterialApp(
-            title: 'GPTGen',
-            debugShowCheckedModeBanner: false,
-            themeMode: themeProvider.themeMode,
-            theme: MyThemes.lightTheme,
-            darkTheme: MyThemes.darkTheme,
-            home: const ChatGPTScreen(),
-          );
-        },
-      );
+Future<void> configLoading() async {
+  EasyLoading.instance
+    ..maskType = EasyLoadingMaskType.none
+    ..loadingStyle = EasyLoadingStyle.dark
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..displayDuration = const Duration(milliseconds: 1000)
+    ..userInteractions = false;
 }
